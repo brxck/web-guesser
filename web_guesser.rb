6990bf43df4dda696a4781dbca8c2ec5aec0358d
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-22 08:35:32 -0700

    Add guess limit.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,24 +1,42 @@
 require "sinatra"
 require "sinatra/reloader"
 
-NUMBER = rand(101)
+class Guesser
+  attr_reader :number, :guesses
 
-def check_guess(guess)
-  if guess == NUMBER
-    message = "You got it right! The secret NUMBER is #{NUMBER}"
-    color = "green"
-  elsif guess < NUMBER
-    message = (guess < NUMBER - 5) ? "Way too low!" : "Too low!"
-    color = (guess < NUMBER - 5) ? "tomato" : "lightcoral"
-  elsif guess > NUMBER
-    message = (guess > NUMBER + 5) ? "Way too high!" : "Too high!"
-    color = (guess > NUMBER + 5) ? "tomato" : "lightcoral"    
+  def initialize
+    @number = rand(101)
+    @guesses = 5
   end
-  [message, color]
+
+  def make_guess(guess)
+    @guesses -= 1
+    if @guesses.zero?
+      initialize
+      return ["You lost!", "red"]
+    end
+    check_guess(guess)
+  end
+
+  def check_guess(guess)
+    if guess == @number
+      message = "You got it right! The secret number is #{@number}"
+      color = "green"
+    elsif guess < @number
+      message = guess < @number - 5 ? "Way too low!" : "Too low!"
+      color = guess < @number - 5 ? "tomato" : "lightcoral"
+    elsif guess > @number
+      message = guess > @number + 5 ? "Way too high!" : "Too high!"
+      color = guess > @number + 5 ? "tomato" : "lightcoral"
+    end
+    [message, color]
+  end
 end
 
-get '/' do
+guesser = Guesser.new
+
+get "/" do
   guess = params["guess"].to_i
-  message, color = check_guess(guess)
+  message, color = guesser.make_guess(guess)
   erb :index, :locals => { :message => message, :color => color }
 end
