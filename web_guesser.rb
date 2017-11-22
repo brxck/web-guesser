0c2270f8f534552ebd90711bb18b0e9ef0a1b8e9
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-21 23:36:56 -0700

    I2: uses an ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 
-random_number = rand(101)
+number = rand(101)
 
 get '/' do
-  "The secret number is #{random_number}."
+  erb :index, :locals => { :number => number }
 end
