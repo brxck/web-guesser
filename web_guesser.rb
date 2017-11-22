98de461388bf5c617904f2a67a70397e9b37a3c7
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-22 00:11:24 -0700

    I4: colored backgrounds.

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,15 +6,19 @@ NUMBER = rand(101)
 def check_guess(guess)
   if guess == NUMBER
     message = "You got it right! The secret NUMBER is #{NUMBER}"
+    color = "green"
   elsif guess < NUMBER
-    message = (guess < NUMBER - 5) ? "Way too low!" : "Too low!" 
+    message = (guess < NUMBER - 5) ? "Way too low!" : "Too low!"
+    color = (guess < NUMBER - 5) ? "tomato" : "lightcoral"
   elsif guess > NUMBER
-    message = (guess > NUMBER + 5) ? "Way too high!" : "Too high!"     
+    message = (guess > NUMBER + 5) ? "Way too high!" : "Too high!"
+    color = (guess > NUMBER + 5) ? "tomato" : "lightcoral"    
   end
+  [message, color]
 end
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => { :message => message }
+  message, color = check_guess(guess)
+  erb :index, :locals => { :message => message, :color => color }
 end
